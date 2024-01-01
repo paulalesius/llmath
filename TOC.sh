dcf863ac6b62b0301a6149a254f5f8776414193e
Paul Alesius <paul@unnservice.com>
2024-01-01 13:58:20 +0100

    Integrate mdbook, TOC.sh fix

--- TOC.sh
+++ TOC.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env sh
 
+# Should be root project dir, we will output to files relative to project
+#SELFPATH=$(dirname $(realpath $0))
+#SUMMARY_ORG=$SELFPATH/src/SUMMARY.org
+
+#echo "* Summary"
+
 cat TOC.tpl | while read line; do
     ITEM=$(echo $line | tr ':' '\n');
     ITEM_LEN=$[$(echo $line | sed 's/:/\n/g' | wc -l)-1];
     PAGENAME=$(echo $line | sed 's/:/\n/g' | tail -n1);
-    PAGEPATH="$(echo $line | sed 's/:/\//g').org";
-    if [ -f "src/$PAGEPATH" ]; then
-        FORMATTED="[[file:$PAGEPATH][$PAGENAME]]"
-    else
-        FORMATTED=$PAGENAME
-    fi
-    echo "$(for i in $(seq 1 $ITEM_LEN); do echo -n -e "\t"; done)$FORMATTED";
+
+    PAGEPATH_ORG="$(echo $line | sed 's/:/\//g').org";
+
+    #if [ -f "src/$PAGEPATH_ORG" ]; then
+        FORMATTED="[[file:$PAGEPATH_ORG][$PAGENAME]]"
+    #else
+    #    FORMATTED=$PAGENAME
+    #fi
+
+    INDENT=$(for i in $(seq 1 $ITEM_LEN); do echo -n -e "\t"; done)
+    echo "$INDENT- $FORMATTED";
 done
